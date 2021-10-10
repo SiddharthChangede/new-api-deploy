c5f906ca3bb80ee8d1b88d5bb500d923f2238a23
Siddharth Changede <123sid231@gmail.com>
2021-10-10 16:46:17 +0530

    final

--- index.js
+++ index.js
@@ -36,4 +36,4 @@ app.get('/getData', (req, res) => {
     })
 })
 
-app.listen(3000, () => { console.log('Listening on port 3000') });
+app.listen(process.env.PORT || 3000, () => { console.log('Listening on port 3000') });
