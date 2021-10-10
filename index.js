d0307401149404e3bb50a7d406fa4ec7f67fb1c3
Siddharth Changede <123sid231@gmail.com>
2021-10-10 16:07:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./nodejs-a8992-firebase-adminsdk-wle41-4aca5326c1.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+var data = {
+    name: 'Cristiano',
+    surname: 'Ronaldo',
+    sport: 'Football'
+}
+
+const db = admin.firestore()
+
+app.post('/addData', (req, res) => {
+    db.collection('users').add(data).then((resp) => {
+        data.id = resp.id
+        db.collection('users').doc(resp.id).update(data).then((resp) => {
+            console.log('Document updated successfully')
+            res.send('Document updated successfully')
+        })
+    })
+})
+
+app.get('/getData', (req, res) => {
+    db.collection('users').get().then((resp) => {
+        const respData = [];
+        for (let i = 0; i < resp.docs.length; i++) {
+            respData.push(resp.docs[i].data())
+        }
+        res.send(respData)
+    })
+})
+
+app.listen(3000, () => { console.log('Listening on port 3000') });
